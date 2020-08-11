153c9b84995d89eb8c48d16b38ef8ff9dc699e06
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-08-11 12:49:12 -0400

    .

--- 2908/2908/main.c
+++ 2908/2908/main.c
@@ -9,7 +9,24 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a, b;
+    int aFlip=0, bFlip=0;
+    scanf("%d %d",&a,&b);
+    for(int i=0;i<3;i++){
+        aFlip*=10;
+        aFlip+=a%10;
+        a/=10;
+    }
+    for(int i=0;i<3;i++){
+        bFlip*=10;
+        bFlip+=b%10;
+        b/=10;
+    }
+    if(aFlip>bFlip){
+        printf("%d",aFlip);
+    }
+    else{
+        printf("%d",bFlip);
+    }
     return 0;
 }
