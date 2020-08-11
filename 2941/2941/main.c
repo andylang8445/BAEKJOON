2bdd75f55b9779c20578eb673fda2f1bc747ecf0
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-08-11 13:37:45 -0400

    .

--- 2941/2941/main.c
+++ 2941/2941/main.c
@@ -9,7 +9,8 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char input[101];
+    scanf("%s",input);
+    
     return 0;
 }
