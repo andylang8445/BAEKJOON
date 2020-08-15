51f89ce9985f2735a8c002a333ae21612f03fd57
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-08-15 00:08:56 -0400

    .

--- 1011/1011/main.c
+++ 1011/1011/main.c
@@ -8,6 +8,8 @@
 
 #include <stdio.h>
 
+int minCnt=2100000000;
+
 int sumUntilN(int num){
     int result;
     result=(num-1)*num;
@@ -26,25 +28,34 @@ int findMaximum(int remainingDistance){
     return result;
 }
 
-long findPath(int distance,int max, int cnt){
+void findPath(int distance,int max, int cnt){
     if(distance==0){
-        
+        if(minCnt>cnt){
+            minCnt=cnt;
+        }
+        return;
     }
     else if(distance<0){
-        return 99999999999;
+        return;
     }
     for(int i=1;i<=max;i++){
-        
+        findPath(distance-i, max, cnt+1);
     }
 }
 
 int main(int argc, const char * argv[]) {
-    int t,x,y,remainingDistance,cnt=0;
+    int t,x,y,remainingDistance,cnt=0,tmp;
     scanf("%d",&t);
     for(;t>0;t--){
         scanf("%d %d",&x,&y);
         remainingDistance=y-x;
         cnt=0;
+        tmp=findMaximum(remainingDistance);
+        cnt=2*tmp-1;
+        remainingDistance-=(sumUntilN(tmp)+sumUntilN(tmp-1));
+        findPath(remainingDistance, tmp, 0);
+        cnt+=minCnt;
+        printf("%d\n",cnt);
     }
     return 0;
 }
