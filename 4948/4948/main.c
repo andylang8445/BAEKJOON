48732cfd72925423a24a010bba523aed199bae63
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-08-28 01:00:10 -0400

    .

--- 4948/4948/main.c
+++ 4948/4948/main.c
@@ -8,19 +8,7 @@
 
 #include <stdio.h>
 
-int arr[1000008];
-
-void filterPrime(int start, int end){
-    for(int i=start;i<=end;i++){
-        //printf("\n\t%d: ",i);
-        for(int j=2;j<=sqrt(i);j++){
-            if(i%j==0){
-                arr[i]++;
-                //printf("%d, ",j);
-            }
-        }
-    }
-}
+int arr[246916];
 
 void filter2(int end){
     for(int i=2;i<end;i++){
@@ -35,17 +23,21 @@ void filter2(int end){
 }
 
 int main(int argc, const char * argv[]) {
-    int m,n;
-    scanf("%d %d",&m,&n);
-    for(int i=1;i<=1000000;i++){
-        arr[i]=0;
-    }
-    filter2(n);
-    //filterPrime(m, n);
-    for(int i=m;i<=n;i++){
-        if(arr[i]==0&&i!=1){
-            printf("%d\n",i);
+    filter2(246912);
+    int input=-1;
+    int cnt;
+    while(1){
+        cnt=0;
+        scanf("%d",&input);
+        if(input==0){
+            break;
         }
+        for(int i=(input+1);i<=(input*2);i++){
+            if(arr[i]==0&&i!=1){
+                cnt++;
+            }
+        }
+        printf("%d\n",cnt);
     }
     return 0;
 }
