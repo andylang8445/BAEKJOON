45d098ef27bde312c6084e252a1579ef3f81d60f
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-09-01 00:51:17 -0400

    .

--- 1002/1002/main.c
+++ 1002/1002/main.c
@@ -7,14 +7,30 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
     int x1,x2,y1,y2,r1,r2;
     int cnt;
+    double d;
     int n;
     scanf("%d",&n);
     for(int i=0;i<n;i++){
-        
+        scanf("%d %d %d %d %d %d",&x1,&y1,&r1,&x2,&y2,&r2);
+        d=sqrt((double)(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))));
+        if(x1==x2&&y1==y2&&r1==r2){
+            printf("-1\n");
+            continue;
+        }
+        else if(d==(double)(r1+r2)||d==(double)abs(r1-r2)){
+            printf("1\n");
+        }
+        else if((d>abs(r2-r1))&&d<(r1+r2)){
+            printf("2\n");
+        }
+        else{
+            printf("0\n");
+        }
     }
     return 0;
 }
