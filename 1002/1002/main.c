a2e2eaf984f3ebc67a22c57864ca4d7803790956
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-09-01 00:25:28 -0400

    .

---
+++ 1002/1002/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  1002
+//
+//  Created by Hongjun Yun on 2020-09-01.
+//  Copyright © 2020 Hongjun Yun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int x1,x2,y1,y2,r1,r2;
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        
+    }
+    return 0;
+}
