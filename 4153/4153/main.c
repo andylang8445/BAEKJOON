30e39a018f9d697c4721b7d751be6aaaedd66a84
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-08-31 23:58:55 -0400

    .

--- 4153/4153/main.c
+++ 4153/4153/main.c
@@ -9,13 +9,28 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int a,b,c;
+    int input[3];
+    int tmp;
     while(1){
-        scanf("%d %d %d",&a,&b,&c);
-        if(a==b&&b==c&&c==0){
+        scanf("%d %d %d",&input[0],&input[1],&input[2]);
+        if((input[0]+input[1]+input[2])==0){
             break;
         }
-        if(
+        for(int i=0;i<2;i++){
+            for(int j=i+1;j<3;j++){
+                if(input[i]>input[j]){
+                    tmp=input[i];
+                    input[i]=input[j];
+                    input[j]=tmp;
+                }
+            }
+        }
+        if(((input[0]*input[0])+(input[1]*input[1]))==(input[2]*input[2])){
+            printf("right\n");
+        }
+        else{
+            printf("wrong\n");
+        }
     }
     return 0;
 }
