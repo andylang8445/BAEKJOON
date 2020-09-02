0afc853208adb7402fee7d5d1f8e535b6447a499
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-09-02 19:57:50 -0400

    .

---
+++ 2447/2447/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  2447
+//
+//  Created by Hongjun Yun on 2020-09-02.
+//  Copyright © 2020 Hongjun Yun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int input;
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
