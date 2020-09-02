1f6bcaf72d737008d86e45e4e0a184f49b19d53a
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-09-02 17:05:34 -0400

    .

---
+++ 10870/10870/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  10870
+//
+//  Created by Hongjun Yun on 2020-09-02.
+//  Copyright © 2020 Hongjun Yun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int function(int current, int end){
+    
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
