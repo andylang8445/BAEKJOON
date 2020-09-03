623c7a6ed16d9dd51a6dfb02f9125503bbc383c7
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-09-02 20:42:53 -0400

    .

--- 2447/2447/main.c
+++ 2447/2447/main.c
@@ -8,9 +8,42 @@
 
 #include <stdio.h>
 
+int arr[2188][2188];
+
+void initialize(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            arr[i][j]=' ';
+    }
+}
+
+void function(int coorx, int coory, int size){
+    if(size==1){
+        arr[coorx][coory]='*';
+        return;
+    }
+    
+    int m=size/3;
+    int cnt=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cnt++;
+            if(cnt!=5)
+                function(coorx+i*m, coory+j*m, m);
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     int input;
-    // insert code here...
-    printf("Hello, World!\n");
+    scanf("%d",&input);
+    initialize(input);
+    function(0, 0, input);
+    for(int i=0;i<input;i++){
+        for(int j=0;j<input;j++){
+            printf("%c",arr[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
