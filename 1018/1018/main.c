ba587aa2eb9a7695a4470683061a7b090e121815
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-10-26 22:33:13 -0400

    .

---
+++ 1018/1018/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  1018
+//
+//  Created by Hongjun Yun on 2020-10-26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
--- 7568/7568/main.c
+++ 7568/7568/main.c
@@ -7,8 +7,34 @@
 
 #include <stdio.h>
 
+struct Person{
+    int x, y, rank;
+};
+
+int N;
+struct Person arr[50];
+
+void myFunction(int num){
+    for(int i=0;i<N;i++){
+        if((arr[i].x>arr[num].x)&&(arr[i].y>arr[num].y)){
+            arr[num].rank++;
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        scanf("%d %d",&arr[i].x,&arr[i].y);
+        arr[i].rank=1;
+    }
+    for(int i=0;i<N;i++){
+        myFunction(i);
+    }
+    
+    for(int i=0;i<N;i++){
+        printf("%d ",arr[i].rank);
+    }
+    
     return 0;
 }
