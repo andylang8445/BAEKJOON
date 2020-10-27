5df9e9c7264b9a68b0962bdf34181d41d14f5a51
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-10-26 21:07:26 -0400

    .

--- 2798/2798/main.c
+++ 2798/2798/main.c
@@ -7,8 +7,24 @@
 
 #include <stdio.h>
 
+int N,M,arr[108];
+int max=0,sum;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    scanf("%d %d",&N,&M);
+    for(int i=0;i<N;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    for(int i=0;i<(N-2);i++){
+        for(int j=i+1;j<(N-1);j++){
+            for(int k=j+1;k<N;k++){
+                sum=arr[i]+arr[j]+arr[k];
+                if((sum<=M)&&(sum>max))
+                    max=sum;
+            }
+        }
+    }
+    printf("%d",max);
     return 0;
 }
