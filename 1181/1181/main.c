33e8f8d383a5dc331792bff0eeeac49753afb625
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2021-12-14 13:42:39 -0500

    .

---
+++ 1181/1181/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  1181
+//
+//  Created by Hongjun Yun on 2021-12-14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
