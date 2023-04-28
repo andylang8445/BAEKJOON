7a2202d4448c914436c15116af849de6a77920c5
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2023-04-28 16:25:30 -0400

    27323

---
+++ 27323/27323/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  27323
+//
+//  Created by Hongjun Yun on 2023-04-28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int a, b;
+    std::cin>>a>>b;
+    std::cout<<a*b;
+    return 0;
+}
