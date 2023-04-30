0bd58a5f8e1014a89202a3fc821bf9c2b48c5483
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2023-04-30 03:38:30 -0400

    24266

---
+++ 24266/24266/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  24266
+//
+//  Created by Hongjun Yun on 2023-04-30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    long int n;
+    long long int output;
+    std::cin>>n;
+    output = n*n*n;
+    std::cout<<output<<std::endl<<3;
+    return 0;
+}
