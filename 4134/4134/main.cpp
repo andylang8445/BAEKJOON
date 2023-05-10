45ed74ed8cb683a991b02a655d8d62a1cdc8cdd1
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2023-05-10 13:50:24 -0400

    4134

---
+++ 4134/4134/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  4134
+//
+//  Created by Hongjun Yun on 2023-05-09.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool is_sosu(long long int a){
+    for(long long int i=2;i<=sqrt(a);i++){
+        if(a%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int in;
+        cin>>in;
+        for(long long int j=in;;j++){
+            if(j>1 && is_sosu(j)){
+                cout<<j<<"\n";
+                break;
+            }
+        }
+    }
+    return 0;
+}
